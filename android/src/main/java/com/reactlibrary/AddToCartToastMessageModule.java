1d40e03b4d49c879c50b7795e6c70372b07e004f
Maksym_Pushkarov <maksym.pushkarov@mckesson.com>
2020-05-18 17:02:16 +0300

    Init commit

--- android/src/main/java/com/reactlibrary/AddToCartToastMessageModule.java
+++ android/src/main/java/com/reactlibrary/AddToCartToastMessageModule.java
@@ -1,15 +1,17 @@
 package com.reactlibrary;
 
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
-import java.util.Map;
 import java.util.HashMap;
+import java.util.Map;
 
 public class AddToCartToastMessageModule extends ReactContextBaseJavaModule {
 
@@ -29,15 +31,23 @@ public class AddToCartToastMessageModule extends ReactContextBaseJavaModule {
     }
 
     @Override
-      public Map<String, Object> getConstants() {
+    public Map<String, Object> getConstants() {
         final Map<String, Object> constants = new HashMap<>();
         constants.put(DURATION_SHORT_KEY, Toast.LENGTH_SHORT);
         constants.put(DURATION_LONG_KEY, Toast.LENGTH_LONG);
         return constants;
-      }
+    }
 
     @ReactMethod
     public void show(String message, int duration) {
-        Toast.makeText(getReactApplicationContext(), message, duration).show();
+        LayoutInflater inflater = LayoutInflater.from(reactContext);
+        View view = inflater.inflate(R.layout.custom_toast, null);
+        TextView toastTextView = (TextView) view.findViewById(R.id.text);
+        toastTextView.setText(message);
+        Toast toast = new Toast(getReactApplicationContext());
+        toast.setView(view);
+        toast.setDuration(duration);
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        toast.show();
     }
 }
